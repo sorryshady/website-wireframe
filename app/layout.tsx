22a4bd07c4f9a0bdf971d8093c416c803b8f35ba
Akhil Nishad <akhilnishadofficial@gmail.com>
2025-02-14 22:49:40 +0530

    added more sizes to icons

--- app/layout.tsx
+++ app/layout.tsx
@@ -246,7 +246,7 @@ export const metadata: Metadata = {
       { url: "/favicon.ico", sizes: "48x48", type: "image/x-icon" },
       { url: "/icon-16x16.png", sizes: "16x16", type: "image/png" },
       { url: "/icon-32x32.png", sizes: "32x32", type: "image/png" },
-      { url: "/icon.png", type: "image/png" },
+      { url: "/icon.png", sizes: "512x512", type: "image/png" },
     ],
     shortcut: "/favicon.ico",
     apple: "/apple-icon.png",
