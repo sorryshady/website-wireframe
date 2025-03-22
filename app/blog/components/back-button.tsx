79b806d54a00ef2599e2394551c2b29cb305bccb
Akhil Nishad <akhilnishadofficial@gmail.com>
2025-03-22 23:59:05 +0530

    fixed the back button behavior

--- app/blog/components/back-button.tsx
+++ app/blog/components/back-button.tsx
@@ -1,14 +1,27 @@
 "use client";
 import { ArrowLeft } from "lucide-react";
+import { useRouter } from "next/navigation";
 import React from "react";
 
 interface BackButtonProps {
   currentPage: "authors" | "blog";
 }
 const BackButton = ({ currentPage }: BackButtonProps) => {
+  const router = useRouter();
+  const clickHandler = () => {
+    if (window.history.length > 1) {
+      window.history.back();
+    } else {
+      if (currentPage === "blog") {
+        router.push("/blog");
+      } else {
+        router.push("/authors");
+      }
+    }
+  };
   return (
     <button
-      onClick={() => window.history.back()}
+      onClick={clickHandler}
       className={`${currentPage === "authors" ? "inline-flex items-center gap-2 text-gray-600 hover:text-gray-900 font-geist" : "inline-flex items-center gap-2 px-4 py-2 bg-gray-100 border border-gray-900 hover:bg-gray-200 hover:border-gray-900 rounded-full transition-colors text-gray-900 font-geist text-sm"} `}
     >
       <ArrowLeft
